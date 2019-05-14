f214705085bcb072ea052270255c1442de641cd3
Алексей Гардер <aleksey_garder@outlook.com>
2019-05-14 18:19:12 +0700

    Main edit

--- Peripherals_Lab3/Program.cs
+++ Peripherals_Lab3/Program.cs
@@ -14,9 +14,6 @@ namespace Peripherals_Lab3
         [STAThread]
         static void Main()
         {
-            var test = DAC.ConvertToAnalog(1, 1, 1, 255);
-
-
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
